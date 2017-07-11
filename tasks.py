01d2cf3eddf6a52afb2b61f4ebe0368928d3087c
Glen Winters <lamouche@gmail.com>
2017-07-11 07:57:25 -0700

    Add logging to science()

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from microsoftbotframework import ReplyToActivity
 
 def science(message):
+    print(message)
     if message['type'] == 'message':
         text = message['text']
         scienced_text = ''.join([c.upper() + '.' for c in text])
