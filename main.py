d325e762c3fe5b9cf43569dac2b4f23202792606
Glen Winters <lamouche@gmail.com>
2017-07-10 22:48:52 -0700

    Add the basic bot code

---
+++ main.py
@@ -0,0 +1,7 @@
+from microsoftbotframework import MsBot
+from tasks import *
+import os
+
+bot = MsBot(port=int(os.environ['PORT']))
+bot.add_process(echo_response)
+bot.run()
---
+++ tasks.py
@@ -0,0 +1,7 @@
+from microsoftbotframework import ReplyToActivity
+
+def science(message):
+    if message['type'] == 'message':
+        text = message['text']
+        scienced_text = ''.join([c.upper() + '.' for c in text])
+        ReplyToActivity(fill=message, text=scienced_text).send()
