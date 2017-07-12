f37c37f3b2222668f66afa4dae3049f3982a1383
Glen Winters <lamouche@gmail.com>
2017-07-12 15:57:41 -0700

    Move message handling to its own function

--- tasks.py
+++ tasks.py
@@ -1,13 +1,21 @@
 from microsoftbotframework import ReplyToActivity
 from utils import sciencify
 
-def message_handler(activity):
+def activity_handler(activity):
     if activity['type'] == 'message':
         message = activity['text']
+        response = _message_handler(message)
+        ReplyToActivity(fill=activity, text=response).send()
+
+def _message_handler(message):
+        mention = '@gvobot'
 
         # Strip mention if it's at the beginning
-        if len(message) >= 7 and message[:7] == '@gvobot':
-            message = message[7:]
+        if message == mention:
+            message = ''
+        elif message.startswith(mention):
+            # Remove the extra space added after the mention, too
+            message = message[len(mention) + 1:]
 
         # Be snarky when no message is sent; otherwise, S.C.I.E.N.C.E.
         if len(message) == 0:
@@ -15,4 +23,5 @@ def message_handler(activity):
                 'even go want to do look more like?'
         else:
             response = sciencify(message)
-        ReplyToActivity(fill=activity, text=response).send()
+
+        return response
