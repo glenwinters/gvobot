a39a4da92e12893ff5cccc963ced3423e50e8db7
Glen Winters <lamouche@gmail.com>
2017-07-12 15:35:29 -0700

    Remove mention from response

--- tasks.py
+++ tasks.py
@@ -4,4 +4,15 @@ from utils import sciencify
 def message_handler(activity):
     if activity['type'] == 'message':
         message = activity['text']
-        ReplyToActivity(fill=activity, text=sciencify(message)).send()
+
+        # Strip mention if it's at the beginning
+        if len(message) >= 7 and message[:7] == '@gvobot':
+            message = message[7:]
+
+        # Be snarky when no message is sent; otherwise, S.C.I.E.N.C.E.
+        if len(message) == 0:
+            response = 'Has anyone really been far even as decided to use ' +
+                'even go want to do look more like?'
+        else:
+            response = sciencify(message)
+        ReplyToActivity(fill=activity, text=response).send()
