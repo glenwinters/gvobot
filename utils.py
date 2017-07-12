bfe8eba11e6b8608eb29cde37a7a0a8be1dc201e
Glen Winters <lamouche@gmail.com>
2017-07-12 15:15:28 -0700

    Improve sciencify by handling periods better

--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
 import string
 
 def sciencify(text):
-    """Uppercase every letter and add a dot after every letter"""
+    """Science that text!
+
+    - Uppercase every letter
+    - Add a period after every letter
+    - Remove periods in the message if they follow a scienced letter
+    """
     scienced_text = ''
-    for c in text.lower():
+    text_lowercase = text.lower()
+    for i, c in enumerate(text_lowercase):
         if c in string.ascii_lowercase:
             scienced_text += c.upper() + '.'
+        elif c == '.':
+            # Don't keep the period if the previous character was a letter
+            if i == 0 or text_lowercase[i - 1] not in string.ascii_lowercase:
+                scienced_text += c
         else:
             scienced_text += c
     return scienced_text
